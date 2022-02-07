3093b2eb7a1f128c7a7a2efc20a3d326a87bb715
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2022-02-06 19:39:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-function setup() {
-    canvas = createCanvas(280, 280);
+function setup(){
+    canvas = createCanvas(300,300);
     canvas.center();
-    background("white");
-    canvas.mouseReleased(classifyCanvas);
-    synth = window.speechSynthesis;
-}
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('MobileNet',modelLoaded);    
+} 
 
-function clearCanvas() {
-    background("white");
+function modelLoaded(){
+     console.log('Model Loaded!');
 }
+ 
+function draw(){
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+var previous_result = '';
 
-function preload() {
-    classifier = ml5.imageClassifier('DoodleNet');
-}
-function draw() {
-    strokeWeight(13);
-    stroke(0);
-    if (mouseIsPressed) {
-        line(pmouseX, pmouseY, mouseX, mouseY);
-    }
-}
-
-function classifyCanvas() {
-    classifier.classify(canvas, gotResult);
-}
-
-function gotResult(error, results) {
-    if (error) {
+function gotResult(error, results){
+    if(error){
         console.error(error);
+    } else {
+        if((results[0].confisence > 0.5) && (previous_result!= results[0].label)){
+                console.log(results);
+                previous_result = results[0].label;
+                var synth = window.speechSynthesis;
+                speak_data = 'Object detected is -' + results[0].label;
+                var utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+                document.getElementById("result_object_name").innerHTML = results[0].label;
+                document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+            }
     }
-    console.log(results);
-    document.getElementById('label').innerHTML = 'label: ' + results[0].label;
-    document.getElementById('confidence').innerHTML = 'confidence: ' + Math.round(results[0].confidence * 100) + '%';
-    utterThis = new SpeechSynthesisUtterance(results[0].label);
-    synth.speak(utterThis);
 }
